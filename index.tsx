fbffca8785e9e5e4020bb1ffbf5dd8ceac4446ce
AthenDrakomin <AthenDrakomin@proton.me>
2025-12-11 16:35:26 +0800

    修复构建错误：移除对已删除pwa-test文件的引用

--- index.tsx
+++ index.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { checkPWAStatus } from './src/pwa-test'
-
-// Check PWA status
-checkPWAStatus()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
